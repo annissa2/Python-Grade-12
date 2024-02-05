40e957d2238e5bde01cd18ba5429a1ecda894569
annissa2 <158535109+annissa2@users.noreply.github.com>
2024-02-05 08:26:57 +0700

    first commit

---
+++ intro.py
@@ -0,0 +1,9 @@
+# this is an introduction to Python
+name = "Anisa"
+money = 100
+
+# this line is not responsible for function
+isAlive = True
+ShopingList = ["apple", 90, True, [1, 2, 3, 4]]
+myDictionary = {"key" : "value", "money" : 100}
+print(name)
